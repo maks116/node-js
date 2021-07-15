c079b0d6b5d351bdc674c178e34966f685a263d5
Maksim <zakiev.maksim@yandex.ru>
2021-07-16 01:46:19 +0300

    added new packages

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
-import Post from './Post.js';
+import router from './router.js'
+import fileUpload from 'express-fileupload'
 
 const PORT = 5000;
 const DB_URL = 'mongodb+srv://user:user@cluster0.bv7rm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
@@ -8,17 +9,9 @@ const DB_URL = 'mongodb+srv://user:user@cluster0.bv7rm.mongodb.net/myFirstDataba
 const app = express();
 
 app.use(express.json());
-
-app.post('/', async (req, res) =>{
-    try{
-        const {author, title, content, picture} = req.body
-        const post = await Post.create({author, title, content, picture})
-        res.json(post)
-    } catch(e){
-        res.status(500).json(e)
-    }
-
-})
+app.use(express.static('static'));
+app.use(fileUpload({}))
+app.use('/api', router)
 
 async function startApp() {
     try{
